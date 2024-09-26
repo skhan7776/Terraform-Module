08724a4c4d201105fd1f0ca3ebad643c825be6c6
skhan7776 <skhan7776@gmail.com>
2024-09-26 19:13:38 +0530

    modified

--- VPC/output.tf
+++ VPC/output.tf
@@ -3,15 +3,14 @@
   //value       = { for vpc in aws_vpc.this : vpc.tags.Name => { "cidr_block" : vpc.cidr_block, "id" : vpc.id } }
 //}
 
-output "vpc_id" {
+output "output_vpc_id" {
   value = aws_vpc.myvpc.id
 }
 
-output "subnet_id" {
+output "vpc_output_subnet_id" {
   value = aws_subnet.public_subnet.id
 }
 
-output "security_groups" {
-  value = aws_vpc.myvpc.id
-  
+output "vpc_output_sg_id" {
+  value = [aws_security_group.web_sg.id]
 }
