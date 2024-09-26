941ddce2007c32c9b3f9d28b79d94b537b31f372
skhan7776 <skhan7776@gmail.com>
2024-09-26 19:15:32 +0530

    modified

--- VPC/VPC.tf
+++ VPC/VPC.tf
@@ -9,7 +9,7 @@ resource "aws_vpc" "myvpc" {
 resource "aws_subnet" "private_subnet" {
   vpc_id = aws_vpc.myvpc.id
   cidr_block = var.private_subnet
-  availability_zone = "1a"  
+  availability_zone = "us-east-1a"  
 }
 
 //creating a Public Subnet
@@ -17,7 +17,7 @@ resource "aws_subnet" "private_subnet" {
 resource "aws_subnet" "public_subnet" {
   vpc_id = aws_vpc.myvpc.id
   cidr_block = var.public_subnet
-  availability_zone = "1a"
+  availability_zone = "us-east-1a"
 }
 
 //creating internet gateway
@@ -35,15 +35,6 @@ resource "aws_route_table" "public_route" {
     gateway_id = aws_internet_gateway.ig.id
   }
 }
-
-resource "aws_route_table" "private_route" {
-  vpc_id = aws_vpc.myvpc.id
-  route {
-    ipv6_cidr_block = var.private_route
-    gateway_id = aws_internet_gateway.ig.id
-  }
-}
-
 //route association
 
 resource "aws_route_table_association" "public_route" {
@@ -52,13 +43,7 @@ resource "aws_route_table_association" "public_route" {
   
 }
 
-resource "aws_route_table_association" "private_route" {
-  subnet_id = aws_subnet.private_subnet.id
-  route_table_id = aws_route_table.private_route.id
-  
-}
-
-resource "security_groups" "web_sg" {
+resource "aws_security_group" "web_sg" {
    name   = "HTTP and SSH"
    vpc_id = aws_vpc.myvpc.id
 
