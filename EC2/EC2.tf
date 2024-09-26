279af2f8770c7935d9526e4d77f2d42985a3149d
skhan7776 <skhan7776@gmail.com>
2024-09-26 19:11:07 +0530

    modified

--- EC2/EC2.tf
+++ EC2/EC2.tf
@@ -2,4 +2,6 @@ resource "aws_instance" "app_server" {
   ami           = var.ami
   instance_type = var.type
   key_name = var.key_pair_name
+  subnet_id = var.subnet_id
+  security_groups = var.security_groups
 }
