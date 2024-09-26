e1831933fe62ee6a78a0f86bba3050195ff4a8d1
skhan7776 <skhan7776@gmail.com>
2024-09-26 19:16:19 +0530

    modified

--- main.tf
+++ main.tf
@@ -1,7 +1,12 @@
-module "servers" {
+module "call_vpc" {
   source = "./VPC"
 }
 
-module "servers1" {
+module "call_ec2" {
   source = "./EC2"
+  ami = "ami-066784287e358dad1"
+  type = "t3.small"
+  subnet_id = module.call_vpc.vpc_output_subnet_id
+  security_groups = module.call_vpc.vpc_output_sg_id
 }
+
